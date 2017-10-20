348dfc19539fe75e5d6911dd0fbfbb906b31aaab
128144 <128144@163.com>
2017-10-20 15:13:16 +0800

    rmon pytest

---
+++ rmon/views/index.py
@@ -0,0 +1,16 @@
+"""rmon.views.index
+"""
+
+from flask import render_template
+from flask.views import MethodView
+
+
+class IndexView(MethodView):
+    """index view
+    """
+
+    def get(self):
+        """
+        """
+
+        return render_template('index.html')
